d8fb5d75519863aedc11e343c070880f217222d3
sing0017 <harmansinghsidhu262@gmail.com>
2023-08-16 13:15:36 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,31 +1,16 @@
-const http = require('http');
-const socketIo = require('socket.io');
+const io = require('socket.io')(5000)
 
-const app = require('express')();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-io.on('connection', (socket) => {
-  const id = socket.handshake.query.id;
-
-  socket.join(id);
+io.on('connection', socket => {
+  const id = socket.handshake.query.id
+  socket.join(id)
 
   socket.on('send-message', ({ recipients, text }) => {
-    recipients.forEach((recipient) => {
-      const newRecipients = recipients.filter((r) => r !== recipient);
-      newRecipients.push(id);
-      io.to(recipient).emit('receive-message', {
-        recipients: newRecipients,
-        sender: id,
-        text,
-      });
-    });
-  });
-});
-
-const HOST = '0.0.0.0';
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, HOST, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+    recipients.forEach(recipient => {
+      const newRecipients = recipients.filter(r => r !== recipient)
+      newRecipients.push(id)
+      socket.broadcast.to(recipient).emit('receive-message', {
+        recipients: newRecipients, sender: id, text
+      })
+    })
+  })
+})
