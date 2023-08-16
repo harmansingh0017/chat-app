5debc09c352f865a27e6a3d947995b42aed8c67e
sing0017 <harmansinghsidhu262@gmail.com>
2023-08-16 18:49:30 -0400

    Update SocketProvider.js

--- client/src/contexts/SocketProvider.js
+++ client/src/contexts/SocketProvider.js
@@ -12,7 +12,7 @@ export function SocketProvider({ id, children }) {
 
   useEffect(() => {
     const newSocket = io(
-      'http://c-s-service:80',
+      'http://localhost:5000',
       { query: { id } }
     )
     setSocket(newSocket)
